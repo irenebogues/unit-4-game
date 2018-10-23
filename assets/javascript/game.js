860d9d4696d572ecccaea89941f99e5cfc43c3f1
izzyme <irenebogues@gmail.com>
2018-10-23 14:07:51 -0400

    working on dynamic divs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,79 +1,103 @@
 // Global Variables
-$(document).ready(function(){
+$(document).ready(function() {
 
 var starWars = {
     countWins: 0,
     countLose: 0,
 
-
 //Star Wars RPG Game characters
   characters: [{
     //Darth Vader
-    id: "star0",
     name: "Darth Vader",
     health: 200,
     attackPower: 10,
     counterPower: 12,
-    imageUrl: '../images/darthvader.png'
+    player: false,
+    enemy: false,
 },
 
 {   //Darth Maul
-    id: "star02",
     name: "Darth Maul",
     health: 180,
     attackPower: 8,
     counterPower: 10,
-    imageUrl: '../images/darthmaul.png'
+    player: false,
+    enemy: false,
+
 },
 
 {   //Obi Wan
-    id: "star03",
     name: "Obi Wan",
     health: 200,
     attackPower: 11,
     counterPower: 13,
-    imageUrl: '../images/obiwan.png'
+    player: false,
+    enemy: false,
 },
 
 {   //Luke Skywalker
-    id: "star04",
     name: "Luke Skywalker",
     health: 210,
     attackPower: 12,
     counterPower: 14,
-    imageUrl: '../images/lukeskywalker.png'
+    player: false,
+    enemy: false,
 },
 
 {   //Yoda
-    id:"star05",
     name: "Yoda",
     health: 250,
     attackPower: 15,
     counterPower: 20,
-    imageUrl: '../images/yoda.png'}
+    player: false,
+    enemy: false,}
 
 ],};
 
-
-
+//Show Characters on div
 for (var i = 0; i < starWars.characters.length; i++) {
     var thisChar = "char" + i;
     var charDiv = $("<div>");
+    charDiv.addClass("char");
     charDiv.attr("id", thisChar);
+    charDiv.attr("data-id", i);
+    charDiv.attr("data-health", i);
     charDiv.attr("data-health", starWars.characters[i].health);
-    $("#gameplayers").append(charDiv);
+    $("#char").append(charDiv);
+//show Health Points on card
+    charHealthDiv.addClass("char-stats-div");
+    charHealthDiv.attr("id", "char-stats" + i);
+    $("#char" + i).append(charHealthDiv);
 
-    var charIcon = $("<i>");
-    $("#char" + i).append(charIcon);
-    var charHealth = $("<span>test</span>");
-    charHealth.html("&nbsp;" + $("#char" + i).attr("data-health"));
-    $("#char" + i).append(charHealth);
+
+    charHealthDiv.attr("id", "char-stats" + i);
+    $("#char" + i).append(charHealthDiv);
+
+
+    var charName = $("<p>");
+    charName.html(starWars.characters[i].name);
+    charName.addClass("bold");
+    $("#char-stats" + i).append(charName);
+
+
+    charIcon.addClass("fa fa-heart");
+    $("#char-stats" + i).append(charIcon);
+
+    var charHealthDiv = $("<span>");
+    $("#char-stats" + i).append(charHealthDiv);
 
 }
+//When characters were chosen to play
+   
+$("#char").on("click", function() {
+    if (starWars.player === 0) {
+        var selectedPlayer = "#char" + $(this).attr("data-id");
+        $("#battlefield").append($(selectedPlayer));
+        starWars.player++;
+        starWars.characters[$(this).attr("data-id")].player = true;
+
+    }
 });
 
-
-
-
-
+});
 
